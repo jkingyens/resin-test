02fb512c6ccbdea10d2c4a73aefd6089fbf850fc
Jeff Kingyens <jeff.kingyens@gmail.com>
2017-05-31 20:18:53 -0700

    test record

--- server.js
+++ server.js
@@ -7,6 +7,41 @@ var camera = new RaspiCam({
   output: '/data/blah.jpg'
 });
 
+var Sound = require('node-arecord');
+ 
+var sound = new Sound({
+ debug: true,    // Show stdout 
+ destination_folder: '/tmp',
+ filename: '/data/recording.wav',
+ alsa_format: 'dat',
+ alsa_device: 'plughw:1,0'
+});
+ 
+sound.record();
+ 
+setTimeout(function () {
+    sound.pause(); // pause the recording after five seconds 
+}, 5000);
+ 
+setTimeout(function () {
+    sound.resume(); // and resume it two seconds after pausing 
+}, 7000);
+ 
+setTimeout(function () {
+    sound.stop(); // stop after ten seconds 
+}, 10000);
+ 
+// you can also listen for various callbacks: 
+sound.on('complete', function() {
+    console.log('Done with recording!');
+
+    var SoundP = require('aplay');
+
+    // fire and forget:
+    new SoundP().play('/data/recording.wav');
+
+});
+
 
 // speaker test
 var say = require('say');
